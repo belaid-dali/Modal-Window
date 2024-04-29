2e748484c73510c8ac6c309c60b83f5d039de8e7
belaid-dali <daliomar.belaid@gmail.com>
2024-04-29 20:11:04 +0100

    handling an Esc keypress Event

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 const openModal = function () {
-    console.log('button clicked');
+    // console.log('button clicked');
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 }
@@ -24,3 +24,10 @@ for(let i = 0; i < btnsOpenModal.length; i++){
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e) {
+    // console.log(e.key);
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModal();
+    }
+})
